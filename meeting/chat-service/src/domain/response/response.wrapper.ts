316028ac85ba6d6f6ae4a2ea38b895399bef3e8e
lukaszfabia <ufabia03@gmail.com>
2025-08-26 12:29:23 +0200

    feat(chat-service): responses wrappers for exceptions

--- meeting/chat-service/src/domain/response/response.wrapper.ts
+++ meeting/chat-service/src/domain/response/response.wrapper.ts
@@ -1,14 +1,20 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { map } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ServiceResponse } from './response';
 import { DTO } from '../dto/dto';
 
 @Injectable()
 export class ServiceResponseWrapperInterceptor<T extends DTO>
   implements NestInterceptor<T, ServiceResponse<T>> {
-  intercept(context: ExecutionContext, next: CallHandler) {
+
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<ServiceResponse<T>> {
     return next.handle().pipe(
-      map((data) => new ServiceResponse(data)),
+      map(data => new ServiceResponse<T>(data)),
+      catchError((err: any) => {
+        const errorMessage = err?.response?.message || err?.message || 'Unknown error';
+        return of(new ServiceResponse<T>(undefined, errorMessage));
+      })
     );
   }
 }
