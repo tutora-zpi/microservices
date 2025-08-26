51a88fa2267dd594a0caab4cbf53559110f575af
lukaszfabia <ufabia03@gmail.com>
2025-08-26 12:26:07 +0200

    feat(chat-service): rename dir and added new handler for getting messages

--- meeting/chat-service/src/app/queries/query.handler.module.ts
+++ meeting/chat-service/src/app/queries/query.handler.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { GetChatHandler } from 'src/app/queries/get-message.query.handler';
+import { GetChatHandler } from 'src/app/queries/get-chat.query.handler';
 import { CHAT_REPOSITORY } from 'src/domain/repository/chat.repository';
 import { DatabaseModule } from 'src/infrastructure/database/database.module';
 import { ChatRepositoryImpl } from 'src/infrastructure/database/repositories/chat.repository.impl';
+import { GetMoreMessagesHandler } from './get-more-messages.handler';
 
-const handlers = [GetChatHandler];
+const handlers = [GetChatHandler, GetMoreMessagesHandler];
 
 @Module({
   imports: [CqrsModule, DatabaseModule],
