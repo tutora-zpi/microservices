58e65df39187826cf136669843f735b27b7e8169
Igor <igopood33@gmail.com>
2025-09-02 18:40:31 +0200

    feat(class-service): exception handling

---
+++ account/class-service/src/main/java/org/tutora/classservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package org.tutora.classservice.exception;
+
+import org.tutora.classservice.dto.ErrorDetailsDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetailsDto> handleResourceNotFoundException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest) {
+
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false).replace("uri=", ""),
+                HttpStatus.NOT_FOUND.value()
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetailsDto> handleGlobalException(
+            Exception exception,
+            WebRequest webRequest) {
+
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(
+                LocalDateTime.now(),
+                "An unexpected internal server error occurred",
+                webRequest.getDescription(false).replace("uri=", ""),
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
