6f335d2e248fee7fe5c96c5ee23e1b9845b0e446
lukaszfabia <ufabia03@gmail.com>
2025-09-03 12:55:04 +0200

    feat(notification-service): repo def

--- alerts/notification-service/internal/domain/repository/notification_repository.go
+++ alerts/notification-service/internal/domain/repository/notification_repository.go
@@ -9,4 +9,5 @@ import (
 type NotificationRepository interface {
 	Save(ctx context.Context, n *models.Notification) (*dto.NotificationDTO, error)
 	MarkAsDelivered(ctx context.Context, id string) error
+	Get(ctx context.Context, receiverID string, lastNotificationID *string, limit int) ([]dto.NotificationDTO, error)
 }
