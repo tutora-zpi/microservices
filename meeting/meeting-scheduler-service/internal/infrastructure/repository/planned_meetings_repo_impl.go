3d00ea382a889444ae39a86ebb9aa500e041a994
lukaszfabia <ufabia03@gmail.com>
2025-10-20 19:04:41 +0200

    fix: meetings and chat

--- meeting/meeting-scheduler-service/internal/infrastructure/repository/planned_meetings_repo_impl.go
+++ meeting/meeting-scheduler-service/internal/infrastructure/repository/planned_meetings_repo_impl.go
@@ -49,7 +49,7 @@ func (p *plannedMeetingsRepoImpl) GetPlannedMeetings(
 		db = db.Where("start_date < ?", req.LastPlannedDate)
 	}
 
-	if err := db.Find(&meetings).Error; err != nil {
+	if err := db.Find(&meetings, req.ClassID).Error; err != nil {
 		return nil, err
 	}
 
